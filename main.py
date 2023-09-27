7b3bac6ba898e324926b05a4ba0e05e53054f60d
unknown <verma199727@gmail.com>
2023-09-27 18:46:09 +0530

    updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import json
+import pandas as pd
+from bs4 import BeautifulSoup
 import prompting
 from PIL import Image
 import streamlit.components.v1 as components
@@ -51,6 +54,47 @@ if clicked:
         with st.spinner("Please wait..."):
             response = prompting.generate_risks(process, num_risks, risk_category)
 
-            st.header(process)
-            risks = response["choices"][0]["message"]["content"]
-            st.write(risks, unsafe_allow_html=True)
+            risks_output = response["choices"][0]["message"]["content"]
+
+            # Parse the JSON response
+            data = json.loads(risks_output)
+            # st.write(risks)
+            # st.write(data)
+
+            # Iterate through the extracted data
+            risks = data["Risks"]
+
+            risk_table_rows = ""
+
+            for risk in risks:
+                risk_title = risk["Risk Title"]
+                risk_category_name = risk["Risk Category"]
+
+                final_response = prompting.generate_risk_information(risk_title, risk_category_name)
+
+                risk_table = final_response["choices"][0]["message"]["content"]
+
+                risk_table_rows = risk_table_rows + risk_table
+
+            html_before = """
+            <table>
+                <tr style="background-color: #000; color: #fff; text-align: center;">
+                    <th>Risk Title</th>
+                    <th>Description</th>
+                    <th>Causes</th>
+                    <th>Financial Impacts</th>
+                    <th>Non-Financial Impacts</th>
+                    <th>Banking Example</th>
+                    <th>Risk Category</th>
+                </tr>
+            """
+
+            html_after = """
+            </table>
+            """
+
+            # Concatenate everything together
+            final_html = html_before + risk_table_rows + html_after
+
+            # Display the HTML table
+            st.write(final_html, unsafe_allow_html=True)
--- prompting.py
+++ prompting.py
@@ -3,28 +3,90 @@ import openai
 import toml
 import streamlit as st
 
-# secrets = toml.load("secrets.toml")
-# openai.api_key = secrets["openai_api_key"]
+secrets = toml.load("secrets.toml")
+openai.api_key = secrets["openai_api_key"]
 
-openai.api_key = st.secrets["openai_api_key"]
-
+# openai.api_key = st.secrets["openai_api_key"]
 
 def generate_risks(process, num_risks, risk_category):
-    text = f"""Imagine you are an Operational Risk expert.
-    Please suggest me {num_risks} Risks for the following process:
-    Proceess Detail: "{process}"
-    The risks that you will provide should belong to the following Risk Categories:
-    Risk Categories: {risk_category}.
-    Please provide the output in an html table with the following information columns:
-    1. Risk Title: The number of words in the risk title should be between 8 to 15 words and should always contain a word associated with threat or a negative event
-    2. Description: Provide a detailed description between 30 to 75 words. Description should contain details of the how the risk can occur. 
-    3. Causes: List the key causes as bullet points
-    4. Financial Impacts: List the key financial impacts as bullet points
-    5. Non-Financial Impacts: List the key non-financial impacts as bullet points
-    6. Banking Example: Provide 1 example of how this risk can occur in a bank. 
-    7. Loss Event Example: Provide 1 real-life example of a loss event that has occurred at another bank related to the risk. 
-    8. Risk Category: Should be one or more from the values provided above.
-    Please style the table header with black background, white font color and center aligned text. Please take you time to think and provide an appropriate response and make sure to only provide the html table and no other additional information."""
+    # Define your JSON format separately
+    json_format = '''
+    {
+        "Risks": [
+            {
+                "Risk Title": "Title of Risk 1",
+                "Risk Category": "Category of Risk 1"
+            },
+            {
+                "Risk Title": "Title of Risk 2",
+                "Risk Category": "Category of Risk 2"
+            },
+            {
+                "Risk Title": "Title of Risk 3",
+                "Risk Category": "Category of Risk 3"
+            },
+            {
+                "Risk Title": "Title of Risk 4",
+                "Risk Category": "Category of Risk 4"
+            },
+            {
+                "Risk Title": "Title of Risk 5",
+                "Risk Category": "Category of Risk 5"
+            },
+            {
+                "Risk Title": "Title of Risk 6",
+                "Risk Category": "Category of Risk 6"
+            }
+        ]
+    }
+    '''
+
+    # Use the JSON format variable in your risk_titles string
+    risk_titles = f"""As an Operational Risk expert, please provide {num_risks} risks associated with the process of {process}. Categorize these risks into the following {risk_category}.
+
+    Present the information in the following JSON format:
+    {json_format}
+
+    Ensure that each risk title contains 8 to 15 words and always includes a word associated with a threat or negative event. Take your time to consider and provide an appropriate response, and include only the risk titles and categories in your answer."""
+
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": risk_titles}],
+        temperature=1,
+        max_tokens=3500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    return response
+
+def generate_risk_information(risk_title, risk_category_name):
+
+    text = f"""
+    Imagine you are an Operational Risk expert.
+    Generate risk information for "{risk_title}" in the following format:
+
+    1. Risk Title: The number of words in the risk title should be between 8 to 15 words and should always contain a word associated with threat or a negative event.
+    2. Description: Provide a detailed description between 30 to 75 words. Description should contain details of how the risk can occur.
+    3. Causes: List the key causes as bullet points.
+    4. Financial Impacts: List the key financial impacts as bullet points.
+    5. Non-Financial Impacts: List the key non-financial impacts as bullet points.
+    6. Banking Example: Provide 1 example of how this risk can occur in a bank.
+    7. Risk Category: Should be one or more from the values: {risk_category_name}.
+
+    Please use this information to construct an HTML table row in the following format:
+
+    <tr>
+        <td>"Risk Title"</td>
+        <td>"Description"</td>
+        <td>"Causes"</td>
+        <!-- Add more <td> elements for Financial Impacts, Non-Financial Impacts, Banking Example, and Risk Category -->
+    </tr>
+
+    Please take your time to generate the information and make sure to only provide the HTML code with no additional information.
+    """
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
